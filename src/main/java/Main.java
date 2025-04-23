57309fa3df83d0ce685ed4dfa5187f9df335d115
AdrianMyszak177257 <177257@stud.prz.edu.pl>
2025-04-23 13:09:21 +0000

    Commit4_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,17 +20,13 @@ class Main {
           System.out.println("Podaj imię studenta:");
           String name = scanner.nextLine();
 
+          System.out.println("Podaj nazwisko studenta:");
+          String surname = scanner.nextLine();
+
           System.out.println("Podaj wiek studenta:");
           int age = scanner.nextInt();
-          scanner.nextLine(); // consume newline
 
-          System.out.println("Podaj numer indeksu:");
-          String studentId = scanner.nextLine();
-
-          System.out.println("Podaj kierunek studiów:");
-          String major = scanner.nextLine();
-
-          s.addStudent(new Student(name, age, studentId, major));
+          s.addStudent(new Student(name, surname, age));
           System.out.println("Dodano studenta!");
           break;
 
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,30 +1,29 @@
 
-public class Student {
-  private String Name;
-  private int Age;
-  private String StudentId;
-  private String Major;
 
-  public Student(String name, int age, String studentId, String major) {
+public class Student {
+
+  private String Name;
+  private String Surname;
+  private int Age;
+
+  public Student(String name, String surname, int age) {
     Name = name;
+    Surname = surname;
     Age = age;
-    StudentId = studentId;
-    Major = major;
   }
 
   public String GetName() {return Name;}
+  public String GetSurname() {return Surname;}
   public int GetAge() {return Age;}
-  public String GetStudentId() {return StudentId;}
-  public String GetMajor() {return Major;}
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age) + " " + StudentId + " " + Major;
+    return Name + " " + Surname + " " + Integer.toString(Age);
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 4) 
-      return new Student("Parse Error", -1, "Unknown", "Unknown");
-    return new Student(data[0], Integer.parseInt(data[1]), data[2], data[3]);
+    if(data.length != 3)
+      return new Student("Parse", "Error", -1);
+    return new Student(data[0], data[1], Integer.parseInt(data[2]));
   }
 }
